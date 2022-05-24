38e206c7a4d05c553c811f4688585937b5614d80
airflow-dags <95067983+airflow-dags@users.noreply.github.com>
2022-05-25 09:33:41 +1000

    Pushed DAG u29-0525-01-0524233340

---
+++ u29-0525-01-0524233340.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+
+from airflow_notebook.pipeline import NotebookOp
+from airflow.utils.dates import days_ago
+
+# Setup default args with older date to automatically trigger when uploaded
+args = {
+    "project_id": "u29-0525-01-0524233340",
+}
+
+dag = DAG(
+    "u29-0525-01-0524233340",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="Created with Elyra 2.2.4 pipeline editor using u29-0525-01.pipeline.",
+    is_paused_upon_creation=False,
+)
+
+
+notebook_op_9717753b_c990_4c21_88ba_4a46474a32e4 = NotebookOp(
+    name="ocp_deploy",
+    namespace="ml-workshop",
+    task_id="ocp_deploy",
+    notebook="ml-workshop/src/automation/deploy_model/ocp/ocp_deploy.py",
+    cos_endpoint="http://minio-ml-workshop:9000/",
+    cos_bucket="airflow",
+    cos_directory="u29-0525-01-0524233340",
+    cos_dependencies_archive="ocp_deploy-9717753b-c990-4c21-88ba-4a46474a32e4.tar.gz",
+    pipeline_outputs=[],
+    pipeline_inputs=[],
+    image="quay.io/ml-aml-workshop/airflow-python-runner:0.0.8",
+    in_cluster=True,
+    env_vars={
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "MODEL_NAME": "u29",
+        "MODEL_VERSION": "1",
+    },
+    config_file="None",
+    dag=dag,
+)
+
+notebook_op_9717753b_c990_4c21_88ba_4a46474a32e4.image_pull_policy = "IfNotPresent"
